2a2941e810c0474b020597ae49f6a49bc8e6be4a
Arnonote <arnonote@gmail.com>
2016-05-03 08:07:36 +0200

    .

--- www/js/directives.js
+++ www/js/directives.js
@@ -2,16 +2,22 @@ var app = angular.module('myApp.directives', []);
 
 app.directive('swCard', function() {
 	return {
+		scope:{title:'@'}, // isolation de scope
 		restrict: 'E',
 		replace: true,
-		transclude: false,
 		template: 	'<div>'
-					+'<img src=""/>'
-					+'<h1>{{name}}</h1>'
-					+'<h2>subtitle</h2>'
+					+'<img src="{{sw_card_src}}"/>'
+					+'<h1>{{title}}</1h1>'
+					+'<h2>{{sw_card_subtitle}}</h2>'
 					+'</div>',
+					
 		link:function(scope, element, attrs){
+			console.log(scope)
+			console.log(element)
+			console.log(attrs)
 			
+			scope.sw_card_subtitle = element.text;
+			scope.sw_card_src = attrs.src;
 		}
 	};
 });
