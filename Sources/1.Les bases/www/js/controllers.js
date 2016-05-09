f836c2df00f3e1b1b8d3f8769b672e3b150810ba
Arnonote <arnonote@gmail.com>
2016-05-09 07:46:35 +0200

    .

--- Sources/1.Les bases/www/js/controllers.js
+++ Sources/1.Les bases/www/js/controllers.js
@@ -1,6 +1,5 @@
 var app = angular.module('myApp.controllers', []);
 
-
 // RootCtrl
 app.controller('RootCtrl', function($scope) 
 {
---
+++ Sources/3.Filtres et services/www/js/app.js
@@ -0,0 +1,2 @@
+var app = angular.module('myApp',['myApp.controllers','myApp.directives','myApp.services']);
+
